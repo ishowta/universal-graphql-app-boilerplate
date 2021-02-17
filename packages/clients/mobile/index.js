f9f6b3597a63d89eb396cc1c174818785cf7e33b
ishowta <ishowta@gmail.com>
2021-02-18 01:43:13 +0900

    dev: commonized app name

--- packages/clients/mobile/index.js
+++ packages/clients/mobile/index.js
@@ -3,5 +3,6 @@
  */
 import { AppRegistry } from "react-native";
 import App from "@app/app/src/App";
+import { name as appName } from "../../../app.json";
 
-AppRegistry.registerComponent("mobile", () => App);
+AppRegistry.registerComponent(appName, () => App);
